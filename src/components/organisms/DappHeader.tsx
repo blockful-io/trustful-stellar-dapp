268356c692b8a53e767b793a1cddf1b3b4d14704
FrancoAguzzi <frankind.eth@gmail.com>
2024-08-12 12:23:09 -0300

    feat: create CardTemplate and DappHeader

---
+++ src/components/organisms/DappHeader.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ConnectWallet } from "./ConnectWallet";
+
+export const DappHeader = () => {
+  return (
+    <div className="w-screen flex items-center mx-auto p-6 max-w-[1216px] h-[72px] justify-between border-t-none border border-r-none border-l-none border-whiteOpacity008">
+      <div className="flex items-center gap-6">
+        <Link href="/">
+          <Image
+            alt="Trustful Stellar Icon"
+            src="/trustful-logo.svg"
+            height={20}
+            width={103}
+            />
+        </Link>
+      </div>
+
+      <ConnectWallet />
+    </div>
+  );
+};

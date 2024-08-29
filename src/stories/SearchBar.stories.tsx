0ab114da3f1a62582bae368f531f1f95ef1b9da2
Luiz Gustavo Abou Hatem De Liz <luizgustavoahsc@gmail.com>
2024-08-29 12:39:34 -0300

    refactor: removing state from search bar and adding it as external property;

--- src/stories/SearchBar.stories.tsx
+++ src/stories/SearchBar.stories.tsx
@@ -1,5 +1,6 @@
 import { SearchBar } from "@/components/molecules/SearchBar";
 import { Meta, StoryObj } from "@storybook/react/*";
+import { useState } from "react";
 
 const meta = {
   title: "TrustfulStellar/SearchBar",
@@ -21,5 +22,15 @@ type Story = StoryObj<typeof meta>;
 export const SearchBarWithNoInput: Story = {
   args: {
     placeholder: "Paste the address...",
+    input: "",
+    onChangeInput: () => {},
+  },
+  render: function (args) {
+    const [value, setValue] = useState("");
+    return (
+      <div className="w-[300px]">
+        <SearchBar {...args} input={value} onChangeInput={setValue} />
+      </div>
+    );
   },
 };

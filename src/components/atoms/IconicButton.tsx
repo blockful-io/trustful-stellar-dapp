e0a97fc32d8128d9c6b9702e500462a65a304741
Luiz Gustavo Abou Hatem De Liz <luizgustavoahsc@gmail.com>
2024-08-29 15:25:26 -0300

    feat: Creating Profile Box with stories; feat: Adding SearchIcon; feat: Adding RotateLeftIcon; feat: Adding darkGreenOpacity01 color;

--- src/components/atoms/IconicButton.tsx
+++ src/components/atoms/IconicButton.tsx
@@ -27,6 +27,7 @@ export const IconicButton = ({
       className={cc([
         "flex w-full space-x-3 items-center justify-center gap-2 px-4 py-2 text-base font-medium text-brandBlack bg-brandGreen rounded-md hover:bg-primary transition",
         { "flex-row-reverse": iconPosition === IconPosition.RIGHT },
+        
         className,
       ])}
     >

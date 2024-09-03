8fc25261d768d04a1a4ea2bcb44b51459ed5ce6a
Luiz Gustavo Abou Hatem De Liz <luizgustavoahsc@gmail.com>
2024-09-03 12:45:42 -0300

    feat: Adding redirect on header to Verify Reputation and Issue Badge screens;

--- src/components/organisms/DappHeader.tsx
+++ src/components/organisms/DappHeader.tsx
@@ -2,12 +2,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { ConnectStellarWallet } from "../auth/ConnectStellarWallet";
 import { useUsersContext } from "../user/Context";
+import { useRouter } from "next/router";
+import cc from "classcat";
 
 export const DappHeader = () => {
   const { userScore } = useUsersContext();
+  const router = useRouter();
+  const path = router.pathname;
   return (
-    <div className="w-screen flex items-center mx-auto p-6 max-w-[100vw] h-[72px] justify-between border-t-none border border-r-none border-l-none border-whiteOpacity008 bg-brandBlack">
-      <div className="flex items-center gap-6">
+    <div className="w-screen flex items-center mx-auto px-6 pb-0 max-w-[100vw] h-[72px] justify-between border-t-none border border-r-none border-l-none border-whiteOpacity008 bg-brandBlack">
+      <div className="flex h-full items-center gap-6">
         <Link href="/">
           <Image
             alt="Trustful Stellar Icon"
@@ -16,6 +20,28 @@ export const DappHeader = () => {
             width={103}
           />
         </Link>
+        {path !== "/" && (
+          <div className="flex h-full">
+            <div
+              className={cc([
+                { "tab-active": path.includes("issue-badge") },
+                "tab p-2 px-4 items-center flex",
+              ])}
+              onClick={() => router.push("/issue-badge")}
+            >
+              <span>Issue Badge</span>
+            </div>
+            <div
+              className={cc([
+                { "tab-active": path.includes("verify-reputation") },
+                "tab p-2 h-full px-4 items-center flex",
+              ])}
+              onClick={() => router.push("/verify-reputation")}
+            >
+              <span>Verify Reputation</span>
+            </div>
+          </div>
+        )}
       </div>
       <div className="flex flex-row">
         {userScore !== undefined ? (

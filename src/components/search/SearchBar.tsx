63e1a000df84d109128be2807d68bf75f332ecc1
Luiz Gustavo Abou Hatem de Liz <50002794+lgahdl@users.noreply.github.com>
2024-09-03 11:45:54 -0300

    Update src/components/search/SearchBar.tsx
    Co-authored-by: caco.eth <49823133+FrancoAguzzi@users.noreply.github.com>

--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -22,7 +22,6 @@ export const SearchBar = (props: SearchBarProps) => {
         value={props.inputText}
         onChange={(e) => props.onChangeInputText(e.target.value)}
         onKeyDown={(event)=>{
-          console.log("here")
           if (event.key === 'Enter') {
             props.onButtonClick(props.inputText)
           }
